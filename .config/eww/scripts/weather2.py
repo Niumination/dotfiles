aff9badd5b5cf40a87f963342ed5005b5aa44eae
niumination <niumination@gmail.com>
2024-11-04 14:33:54 +0700

    push all config

---
+++ .config/eww/scripts/weather2.py
@@ -0,0 +1,20 @@
+import subprocess, json, os
+
+def kelvinToCelsius(kelvin):
+    return round(kelvin) - 273
+
+path = os.path.dirname(os.path.abspath(__file__))
+res = subprocess.run([f'{path}/getweather'], stdout=subprocess.PIPE)
+decoded = json.loads(res.stdout.decode('utf-8'))
+
+temp_k = decoded["main"]["temp"]
+temp_c = round(kelvinToCelsius(temp_k))
+weather = decoded["weather"][0]["main"]
+print(f'''
+(box :class "weather1" :space-evenly false :halign "center"
+(image :path "./images/dark/{decoded["weather"][0]["icon"]}.png" :image-width  "40")
+(box :class "weather2" :orientation "h"
+"{temp_c}°, {weather}"
+)
+)
+''')
